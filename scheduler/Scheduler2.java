7d65d6869b2c21e25dc566aad035c245feea8baa
alexpena9291 <alexpena9291@gmail.com>
2021-11-27 11:49:34 -0500

    feat(architecture): adding in provided code

---
+++ scheduler/Scheduler2.java
@@ -0,0 +1,24 @@
+package scheduler;
+
+/**
+ * A stub for your secondScheduler1.java scheduler code
+ */
+public class Scheduler2 implements Scheduler {
+
+	/**
+	 * @see scheduler.Scheduler#authors()
+	 */
+	public String authors() {
+		// TODO Your Code Here!
+		return null;
+	}
+
+	/**
+	 * @see scheduler.Scheduler#schedule(scheduler.SchedulingProblem)
+	 */
+	public ScheduleChoice[] schedule(SchedulingProblem pProblem) {
+		// TODO Your Code Here!
+		return null;
+	}
+
+}
